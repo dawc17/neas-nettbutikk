2435ab6c7e953d7ac95bbe21d99c66425b3b115b
daw_c17 <100421659+dawc17@users.noreply.github.com>
2025-03-05 15:26:12 +0100

    Update Footer.jsx

--- neasbutikk/src/components/Footer.jsx
+++ neasbutikk/src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import { NavbarButton } from "./Navbar";
+
 function FooterMain() {
   return (
-    <div>
-      <h1>SIGMABOY</h1>
+    <div className="footer-wrapper">
+      <div className="bg-pinegreen text-white flex flex-col rounded-xl gap-2 w-full items-center shadow-lg p-4">
+        <div className="flex flex-row w-full justify-between items-center p-4 bg-pinegreen-footer rounded-xl"></div>
+
+        <div className="grid grid-cols-4 gap-2 w-full">
+          <div className="col-span-2 flex items-center justify-center p-4 bg-pinegreen-footer rounded-xl"></div>
+          <div className="flex items-center justify-center p-4 bg-pinegreen-footer rounded-xl"></div>
+          <div className="flex items-center justify-center p-4 bg-pinegreen-footer rounded-xl"></div>
+        </div>
+
+        <div className="flex flex-row w-full justify-between items-center p-4 bg-mossgreen rounded-xl"></div>
+      </div>
     </div>
   );
 }
