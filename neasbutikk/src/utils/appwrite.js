6c2f38a48d3d782ea808d07177632d93ce1f476f
dawc17 <dawid.czaplicki2008@gmail.com>
2025-05-05 10:37:25 +0200

    ewgewg

--- neasbutikk/src/utils/appwrite.js
+++
@@ -1,9 +0,0 @@
-import { Client, Account, Databases } from "appwrite";
-
-const client = new Client();
-client
-  .setEndpoint("https://cloud.appwrite.io/v1") // Appwrite Cloud endpoint
-  .setProject("neas"); // Replace with your Appwrite project ID
-
-export const account = new Account(client);
-export const databases = new Databases(client);
